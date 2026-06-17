c8ab117e234bee5693eea80c64a5ae67f93eec2a
Moris Takhellambam <moris.takhel@gmail.com>
2026-06-17 11:21:28 +0530

    update config

--- config.py
+++
@@ -1,2 +0,0 @@
-RAW_DATA_PATH=r"/home/moris/Projects/expense/data/Monefy.Data.6-16-26.csv"
-CLEANED_DATA_PATH=r"/home/moris/Projects/expense/data/cleaned_data.csv"
---
+++ config_example.py
@@ -0,0 +1,2 @@
+RAW_DATA_PATH=/your/path/to/raw_data.csv
+CLEANED_DATA_PATH=/your/path/to/cleaned_data.csv
